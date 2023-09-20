ad19391c89582503329cb87fe4301e8e2d6c931a
M.Baumgärtner <klagges2011@googlemail.com>
2023-09-20 15:45:14 +0200

    Updates for Building Jar with dependency

--- src/main/java/dev/baumgaertner/multipletool/Utils/CheckForValidDate.java
+++ src/main/java/dev/baumgaertner/multipletool/Utils/CheckForValidDate.java
@@ -1,23 +1,19 @@
 package dev.baumgaertner.multipletool.Utils;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.GregorianCalendar;
 
 public class CheckForValidDate {
 
-    public static boolean checkIsValid(int year, int month, int day){
-        System.out.println(" Eingang : " +day +" " + month + " " + year);
-
+    public static boolean checkIsValid(int year, int month, int day) {
         try {
             LocalDate myDate = LocalDate.of(year, month, day);
-
-           LocalDate my = LocalDate.of(1981, 1,9);
-            System.out.println(my);
-           return true;
-        } catch (RuntimeException e ){
+            return true;
+        } catch (RuntimeException e) {
             System.out.println(" Fehler");
         }
-       return false;
+        return false;
     }
 
     public static boolean isSchaltjahr(int year) {
